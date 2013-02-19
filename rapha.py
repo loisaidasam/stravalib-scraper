286b91715906a65680e7736b7c976f80a7ddede7
sam <sam.sandberg@zemanta.com>
2013-02-18 20:17:22 -0500

    proof of concept i wrote to see if i could scrape the rapha rising challenge data

---
+++ rapha.py
@@ -0,0 +1,45 @@
+import cookielib
+import time
+import urllib
+import urllib2
+
+from BeautifulSoup import BeautifulSoup
+
+TIME_BT_REQUESTS = 2
+EMAIL = "email@example.com"
+PASSWORD = "yourpasswordhere"
+
+def main():
+	print "Logging in..."
+	cj = cookielib.CookieJar()
+	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+
+	f = opener.open('https://www.strava.com/login')
+	soup = BeautifulSoup(f.read())
+
+	time.sleep(TIME_BT_REQUESTS)
+
+	utf8 = soup.findAll('input', {'name': 'utf8'})[0].get('value').encode('utf-8')
+	token = soup.findAll('input', {'name': 'authenticity_token'})[0].get('value')
+
+	values = {
+		'utf8': utf8,
+		'authenticity_token': token,
+		'email': EMAIL,
+		'password': PASSWORD,
+	}
+	data = urllib.urlencode(values)
+	url = 'https://www.strava.com/session'
+	response = opener.open(url, data)
+	soup = BeautifulSoup(response.read())
+
+	print "Logged in, let's see what's good..."
+
+	time.sleep(TIME_BT_REQUESTS)
+
+	f = opener.open('http://app.strava.com/challenges/rapha-rising-circle-of-death/')
+	soup = BeautifulSoup(f.read())
+
+	print soup
+
+main()
